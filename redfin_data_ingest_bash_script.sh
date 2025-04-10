a30c66173108bd37db5a1e135d94ebdc90a338ba
Donatus Enebuse <118270006+vaxdata22@users.noreply.github.com>
2025-04-10 20:47:11 +0100

    Rename redfin_data_ingest_bash_script.sh to codes/redfin_data_ingest_bash_script.sh

--- redfin_data_ingest_bash_script.sh
+++
@@ -1,1 +0,0 @@
-wget -O - https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_tracker/city_market_tracker.tsv000.gz | aws s3 cp - s3://redfin-analytics-emr-etl-bucket/raw-data/city_market_tracker.tsv000.gz
